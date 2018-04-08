06e3c3bcb8c49b747941cfaf54400f6c2f1c1531
Unknown <daichizaki@yahoo.co.jp>
2018-04-09 01:11:55 +0900

    kansu

--- yukariVoice.js
+++ yukariVoice.js
@@ -1,7 +1,7 @@
 const googlehome = require('google-home-notifier');
 const language = 'ja';
 
-function playVoice(message){
+exports.playVoice = function(message){
     googlehome.device("Google-Home", language);
     googlehome.ip("192.168.179.5");
     googlehome.play('http://192.168.179.7:3000/yukariVoice/' + message, function(res) {
