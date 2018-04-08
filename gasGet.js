6d145cec42e875f47a45f0d4d632cb139dbbfd02
Unknown <daichizaki@yahoo.co.jp>
2018-04-08 23:42:46 +0900

    t

--- gasGet.js
+++ gasGet.js
@@ -7,10 +7,11 @@ var dt = new Date();
 
 console.log(dt.toFormat("YYYY/MM/DD/HH24時MI分SS秒"));
 const URL = 'https://script.google.com/macros/s/AKfycbwJYNVMICtqDNwIsXS50LIsFEoPC_XeN41Eljj6o5_G2DjXmjU/exec?kitaku=帰宅&zikan=' + dt.toFormat("YYYY/MM/DD HH24時MI分SS秒");
-https.get(URL, function (res) {
-    console.log(URL);
-});
-
 request.get(URL, function(err, res, body) {
-  console.log(body);
+    if (err) {
+      console.log('Error: ' + err.message);
+      return;
+    } else {
+      console.log(body);
+    }
 });
